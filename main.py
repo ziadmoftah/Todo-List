f8aaa955bf23e3815f1b7ad013acbfaf76c4e9ef
Ziad Moftah <43881897+ziadmoftah@users.noreply.github.com>
2025-06-12 15:36:37 +0300

    Delete old unused schemas, controllers, models

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from entities import subtask,task,priority,list
-from routers import subtask_controller, task_controller, list_controller
 from database.core import  engine
 from todos.subtasks import controllers as new_subtask_controllers
 from todos.tasks import controllers as new_task_controllers
 from todos.lists import controllers as new_list_controllers
-from database.core import DbSession
 
 
 
@@ -16,9 +14,6 @@ priority.Base.metadata.create_all(bind=engine)
 list.Base.metadata.create_all(bind=engine)
 
 
-app.include_router(list_controller.router)
-app.include_router(task_controller.router)
-app.include_router(subtask_controller.router)
 app.include_router(new_subtask_controllers.router)
 app.include_router(new_task_controllers.router)
 app.include_router(new_list_controllers.router)

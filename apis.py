02bb0b34efafeb5b5434a93e0a8793978df69232
Ziad Moftah <43881897+ziadmoftah@users.noreply.github.com>
2025-06-13 14:24:40 +0300

    Added api file for all apis controllers

---
+++ apis.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from todos.lists import controllers as list_controllers
+from todos.tasks import controllers as task_controllers
+from todos.subtasks import controllers as subtask_controllers
+
+def register_routes(app : FastAPI):
+    app.include_router(list_controllers.router)
+    app.include_router(task_controllers.router)
+    app.include_router(subtask_controllers.router)
--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 from fastapi import FastAPI
-from entities import subtask,task,priority,list
-from database.core import  engine
-from todos.subtasks import controllers as new_subtask_controllers
-from todos.tasks import controllers as new_task_controllers
-from todos.lists import controllers as new_list_controllers
+from apis import register_routes
+from database.core import  engine, Base
 
 
 
 app = FastAPI(title="Todo list")
-subtask.Base.metadata.create_all(bind=engine)
-task.Base.metadata.create_all(bind=engine)
-priority.Base.metadata.create_all(bind=engine)
-list.Base.metadata.create_all(bind=engine)
-
-
-app.include_router(new_subtask_controllers.router)
-app.include_router(new_task_controllers.router)
-app.include_router(new_list_controllers.router)
-
-
-@app.get("/HelloWorld")
-def read_root():
-    return {"Hello World"}
-
-
-
-
-
+Base.metadata.create_all(bind=engine)
+register_routes(app)
